b7f4063c2ff8a2abba817826a585748e6bd49dd5
WMascot <danila33366@gmail.com>
2023-03-17 22:48:02 +0300

    Removed hosted service

--- MessageSender.Console/Program.cs
+++ MessageSender.Console/Program.cs
@@ -1,7 +1,7 @@
-﻿using MessageSender.Console;
-using MessageSender.Console.Interfaces;
+﻿using MessageSender.Console.Interfaces;
 using MessageSender.Console.Models;
 using MessageSender.Console.Services;
+using MessageSender.DAL.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,8 +12,18 @@ class Program
 {
     public static async Task<int> Main(string[] args)
     {
-        var host = CreateHostBuilder(args);
-        await host.RunConsoleAsync();
+        var host = CreateHostBuilder(args).Build();
+
+        var fileService = host.Services.GetRequiredService<IFileService>();
+        var mailService = host.Services.GetRequiredService<IMailService>();
+
+        using (var scope = host.Services.CreateScope())
+            await scope.ServiceProvider.GetRequiredService<DbInitializer>().InitializeAsync().ConfigureAwait(false);
+
+        var messageFiles = await fileService.ReadMessageFilesAsync();
+        var sendedMessages = await mailService.SendAsync(messageFiles);
+
+        await fileService.ChangeMessageFilesStatusAsync(sendedMessages);
         return Environment.ExitCode;
     }
     private static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -33,9 +43,9 @@ class Program
                 if (hostContext.HostingEnvironment.IsDevelopment())
                     builder.AddUserSecrets<Program>();
             })
-            .ConfigureServices((hostContext, services) =>
+            .ConfigureServices(async (hostContext, services) =>
             {
-                services.AddHostedService<Worker>()
+                services.AddDataBase(hostContext.Configuration)
                         .AddTransient<IFileService, FileService>()
                         .AddTransient<IMailService, MailService>();
 
