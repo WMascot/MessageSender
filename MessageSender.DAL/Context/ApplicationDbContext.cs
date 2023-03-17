d959177d303f5656a4ecaf14edb253957dfad58e
WMascot <danila33366@gmail.com>
2023-03-17 18:04:31 +0300

    Added context

---
+++ MessageSender.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using MessageSender.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageSender.DAL.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasKey(x => new { x.Id, x.Email });
+            modelBuilder.Entity<Professor>()
+                .HasKey(x => new { x.Id, x.Email });
+            modelBuilder.Entity<Event>()
+                .HasKey(x => new { x.Id, x.Name });
+            modelBuilder.Entity<StudyYear>()
+                .HasKey(x => new { x.Id, x.Year });
+
+
+            modelBuilder.Entity<Student>()
+                .HasOne(x => x.Professor)
+                .WithMany(x => x.Students)
+                .HasForeignKey(x => x.ProfessorId)
+                .HasPrincipalKey(x => x.Id);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(x => x.StudyYear)
+                .WithMany(x => x.Students)
+                .HasForeignKey(x => x.StudyYearId)
+                .HasPrincipalKey(x => x.Id);
+
+            modelBuilder.Entity<Student>()
+                .HasOne(x => x.Event)
+                .WithMany(x => x.Students)
+                .HasForeignKey(x => x.EventId)
+                .HasPrincipalKey(x => x.Id);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<StudyYear> StudyYears { get; set; }
+    }
+}
---
+++ MessageSender.DAL/Services/DbRegister.cs
@@ -0,0 +1,19 @@
+﻿using MessageSender.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MessageSender.DAL.Services
+{
+    public static class DbRegister
+    {
+        public static IServiceCollection AddDataBase(this IServiceCollection services, IConfiguration configuration) => services
+            .AddDbContext<ApplicationDbContext>(opt =>
+            {
+                var connectionString = configuration.GetConnectionString("MySQL");
+                opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            })
+            .AddTransient<DbInitializer>()
+            .AddRepositories();
+    }
+}
