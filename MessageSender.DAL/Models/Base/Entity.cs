6a0a17750fb04d32bdecd21bf70c5016ae64f46f
WMascot <danila33366@gmail.com>
2023-03-17 22:47:43 +0300

    Entity id generation fix

--- MessageSender.DAL/Models/Base/Entity.cs
+++ MessageSender.DAL/Models/Base/Entity.cs
@@ -1,9 +1,11 @@
 ﻿using MessageSender.DAL.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MessageSender.DAL.Models.Base
 {
     public class Entity : IEntity
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
